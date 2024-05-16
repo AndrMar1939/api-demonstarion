686d42eb80117f03b154431d6fd6aa5de99f345e
Andrii Marusiak <advo737@gmail.com>
2024-05-16 17:14:21 +0300

    feat/ added google api

---
+++ src/features/Translation.tsx
@@ -0,0 +1,67 @@
+'use client'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+
+const TEXT_TO_TRANSLATE = [
+  'An old silent pond...',
+  'A frog jumps into the pond, splash!',
+  'Silence again.',
+]
+
+const translate = async (text: string) => {
+  let res = await axios.post(
+  `https://translation.googleapis.com/language/translate/v2?key=${process.env.NEXT_PUBLIC_GOOGLE_TRANSLATE_KEY}`,
+  { q: text, source: 'en', target: "de" }
+  );
+
+  console.log('api google')
+
+  let translation = res.data.data.translations[0].translatedText;
+  return translation;
+}
+
+function Translation() {
+  const [initText, setInitText] = useState('')
+  const [translatedText, setTranslatedText] = useState('')
+
+  console.log('rerender')
+
+  useEffect(() => {
+    if (initText) {
+      translate(initText).then(text => {
+        setTranslatedText(text)
+      })
+    }
+  }, [initText])
+
+  return (
+    <div className='flex flex-col gap-10 text-2xl items-center'>
+      <p className='mb-[60px] rounded-xl bg-white p-9 flex'>
+        {TEXT_TO_TRANSLATE
+          .map(word => 
+            (<span 
+                className='hover:bg-slate-200 rounded-full p-1'
+                key={word}
+                onClick={(e: any) => {
+                  const text = e.target.innerText
+                  setInitText(text)
+                  setTranslatedText('')
+                }}
+              >
+                {word}
+              </span>
+          ))}
+      </p>
+
+    {initText && <p className='text-5xl font-bold'>{initText}</p>}
+
+    {initText && <p className='font-bold'>---</p>}
+
+    {initText && !translatedText && <p className='text-5xl font-bold'>loading...</p>}
+
+    {translatedText && <p className='text-5xl font-bold'>{translatedText}</p>}
+    </div>
+  )
+}
+
+export default Translation
