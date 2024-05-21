53a46fece931ce7ab2c0f444f45ad37136fe6001
Andrii Marusiak <advo737@gmail.com>
2024-05-21 18:01:00 +0300

    fix: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-      <h1 className="text-3xl font-semibold">Translators and Dictionaries</h1>
+      <h1 className="text-3xl font-semibold mx-auto w-fit">Translators and Dictionaries</h1>
   );
 }
