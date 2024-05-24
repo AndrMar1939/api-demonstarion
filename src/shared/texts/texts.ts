edb9f995e1dc554dac05052f065c62bd002acf0d
Andrii Marusiak <advo737@gmail.com>
2024-05-24 13:14:36 +0300

    chore: add prettier

--- src/shared/texts/texts.ts
+++ src/shared/texts/texts.ts
@@ -1,11 +1,13 @@
-import type { TextInLibrary } from "@/types"
+import type { TextInLibrary } from '@/types';
 
 export const textSmall: TextInLibrary = {
   id: 1,
-  content: 'Deutschland gehört vollständig zur gemäßigten Klimazone Mitteleuropas im Bereich der Westwindzone und befindet sich im Übergangsbereich zwischen dem Seeklima in Westeuropa und dem Kontinentalklima in Osteuropa.',
-}
+  content:
+    'Deutschland gehört vollständig zur gemäßigten Klimazone Mitteleuropas im Bereich der Westwindzone und befindet sich im Übergangsbereich zwischen dem Seeklima in Westeuropa und dem Kontinentalklima in Osteuropa.',
+};
 
 export const textMedium: TextInLibrary = {
   id: 2,
-  content: 'Die deutsche Schriftstellerin Jenny Erpenbeck hat den International Booker Prize gewonnen. Zusammen mit ihrem Übersetzer Michael Hofmann erhielt sie den bedeutenden britischen Literaturpreis für ihren Roman "Kairos". Mit dem International Booker Prize werden fremdsprachige Werke, die ins Englische übersetzt wurden, ausgezeichnet. Jenny Erpenbecks Roman "Kairos" von 2021 ist das erste im Original deutschsprachige Buch, das diesen Preis erhält.'
-}
+  content:
+    'Die deutsche Schriftstellerin Jenny Erpenbeck hat den International Booker Prize gewonnen. Zusammen mit ihrem Übersetzer Michael Hofmann erhielt sie den bedeutenden britischen Literaturpreis für ihren Roman "Kairos". Mit dem International Booker Prize werden fremdsprachige Werke, die ins Englische übersetzt wurden, ausgezeichnet. Jenny Erpenbecks Roman "Kairos" von 2021 ist das erste im Original deutschsprachige Buch, das diesen Preis erhält.',
+};
